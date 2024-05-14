7f2d7a19baba5e92b1701f0c7119f0bc354da913
denhampreen <denhampreen@gmail.com>
2024-05-14 10:50:24 +0200

    async

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,42 +1,36 @@
 import { ETHRegistrarControllerContract, EnsHandleEntity } from "generated";
 
-ETHRegistrarControllerContract.NameRegistered.loader(
-  ({ event, context }) => {}
+ETHRegistrarControllerContract.NameRegistered.handlerAsync(
+  async ({ event, context }) => {
+    const name = event.params.name.replace(/\u0000/g, "").slice(0, 255);
+
+    const ensHandle: EnsHandleEntity = {
+      id: name,
+      label: event.params.label,
+      owner: event.params.owner,
+      cost: event.params.cost,
+      expires: event.params.expires,
+    };
+
+    context.EnsHandle.set(ensHandle);
+  }
 );
 
-ETHRegistrarControllerContract.NameRegistered.handler(({ event, context }) => {
-  const name = event.params.name.replace(/\u0000/g, "").slice(0, 255);
+ETHRegistrarControllerContract.NameRenewed.handlerAsync(
+  async ({ event, context }) => {
+    const name = event.params.name.replace(/\u0000/g, "").slice(0, 255);
 
-  const ensHandle: EnsHandleEntity = {
-    id: name,
-    label: event.params.label,
-    owner: event.params.owner,
-    cost: event.params.cost,
-    expires: event.params.expires,
-  };
+    const ensHandle = await context.EnsHandle.get(name);
 
-  context.EnsHandle.set(ensHandle);
-});
-
-ETHRegistrarControllerContract.NameRenewed.loader(({ event, context }) => {
-  context.EnsHandle.load(
-    event.params.name.replace(/\u0000/g, "").slice(0, 255)
-  );
-});
-
-ETHRegistrarControllerContract.NameRenewed.handler(({ event, context }) => {
-  const name = event.params.name.replace(/\u0000/g, "").slice(0, 255);
-
-  const ensHandle = context.EnsHandle.get(name);
-
-  if (ensHandle === undefined) {
-    // it seems some expiration events are emitted before the registration event?
-    // todo dig into
-    // throw new Error(`Can't renew an EnsHandle that doesn't exist: ${name}`);
-  } else {
-    context.EnsHandle.set({
-      ...ensHandle,
-      expires: event.params.expires,
-    });
+    if (ensHandle === undefined) {
+      // it seems some expiration events are emitted before the registration event?
+      // todo dig into
+      // throw new Error(`Can't renew an EnsHandle that doesn't exist: ${name}`);
+    } else {
+      context.EnsHandle.set({
+        ...ensHandle,
+        expires: event.params.expires,
+      });
+    }
   }
-});
+);
